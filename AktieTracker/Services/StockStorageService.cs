510042cc8e3c3215ff1316c13663775f430299fd
ThomasM52 <58251@student.eadania.dk>
2026-03-06 12:59:55 +0100

    Add project files.

---
+++ AktieTracker/Services/StockStorageService.cs
@@ -0,0 +1,31 @@
+﻿using AktieTracker.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace AktieTracker.Services
+{
+    public class StockStorageService
+    {
+        private const string FileName = "stocks.json";
+
+        public void Save(IEnumerable<Stock> stocks)
+        {
+            var json = JsonSerializer.Serialize(stocks, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText(FileName, json);
+        }
+
+        public List<Stock> Load()
+        {
+            if (!File.Exists(FileName))
+                return new List<Stock>();
+
+            var json = File.ReadAllText(FileName);
+            return JsonSerializer.Deserialize<List<Stock>>(json) ?? new List<Stock>();
+        }
+    }
+}
